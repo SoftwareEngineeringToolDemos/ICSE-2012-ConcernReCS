75733a8e0eb69a45a97288adb47e5d3dc6a78c64
emagno <emagno@ecc854d0-6853-4960-84ac-11c72213e77b>
2011-09-03 02:29:07 +0000

    Comments.

--- ConcernReCS/src/ufmg/crcs/ConcernReCS.java
+++ ConcernReCS/src/ufmg/crcs/ConcernReCS.java
@@ -1,3 +1,21 @@
+/*
+ * Federal University of Minas Gerais 
+ * Department of Computer Science
+ * ConcernReCS Project
+ *
+ * Created by Pericles Alves
+ * Date: 01/08/2011
+ */
+
+// TODO [EF] Pericles e Diogo, sugiro adotar o cabeçalho acima em todos os arquivos.
+// Para o cabeçalho ser adicionado automaticamente pelo Eclipse, edite:
+// Window -> Preferences -> Java -> Code Style -> Code Templates -> Comments -> Files
+
+// TODO [EF] Comentem as classes e métodos usando o estilo Java Doc, ou seja: /** comentário */.
+
+// TODO [EF] Estou usando a tag [EF] para identificar meus comentários (EF = Eduardo Figueiredo).
+// Quando acharem necessário se identificar, vcs podem fazer o mesmo: [PA] = Péricles e [DM] = Diogo.
+
 //ConcernReCS is a plug-in to find code smells in order to guide concerns refactoring
 
 package ufmg.crcs;
